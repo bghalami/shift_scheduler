b4abcb63d85ffd249ba4f5eb8f4c3740b47f3e61
bghalami <benjaminghalami@gmail.com>
2019-09-19 11:03:41 -0600

    Added simplecov to check test coverage

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -5,6 +5,9 @@ require File.expand_path('../../config/environment', __FILE__)
 abort("The Rails environment is running in production mode!") if Rails.env.production?
 require 'rspec/rails'
 
+require 'simplecov'
+SimpleCov.start
+
 begin
   ActiveRecord::Migration.maintain_test_schema!
 rescue ActiveRecord::PendingMigrationError => e
