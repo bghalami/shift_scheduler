5ee52863f8d756feefee96e69c517cdbd1e538ba
bghalami <benjaminghalami@gmail.com>
2019-10-09 17:44:29 -0600

    Created schema for users and routes for admin/users

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   namespace :admin do
     resources :companies, only: [:destroy, :index, :new, :create, :show]
+    resources :users, only: [:destroy, :index, :new, :create, :show]
   end
 
   resources :companies, only: [:show]
--- db/schema.rb
+++ db/schema.rb
@@ -1,9 +1,34 @@
-ActiveRecord::Schema.define(version: 2019_09_19_162728) do
+# This file is auto-generated from the current state of the database. Instead
+# of editing this file, please use the migrations feature of Active Record to
+# incrementally modify your database, and then regenerate this schema definition.
+#
+# Note that this schema.rb definition is the authoritative source for your
+# database schema. If you need to create the application database on another
+# system, you should be using db:schema:load, not running all the migrations
+# from scratch. The latter is a flawed and unsustainable approach (the more migrations
+# you'll amass, the slower it'll run and the greater likelihood for issues).
+#
+# It's strongly recommended that you check this file into your version control system.
 
+ActiveRecord::Schema.define(version: 2019_10_09_233656) do
+
+  # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
   create_table "companies", force: :cascade do |t|
     t.string "title"
   end
 
+  create_table "user_tables", force: :cascade do |t|
+    t.string "name"
+    t.string "username"
+    t.string "password_digest"
+    t.float "pay_rate"
+    t.integer "hours_requested"
+    t.integer "role"
+    t.bigint "company_id"
+    t.index ["company_id"], name: "index_user_tables_on_company_id"
+  end
+
+  add_foreign_key "user_tables", "companies"
 end
