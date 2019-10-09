267035d953196f3195e7a05c5c10a9ae09fb69c0
bghalami <benjaminghalami@gmail.com>
2019-10-09 17:51:45 -0600

    Created base setup for userscontroller

---
+++ app/controllers/admin/users_controller.rb
@@ -0,0 +1,27 @@
+class Admin::UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+  end
+  def index
+    @users = User.all
+  end
+  def new
+    @user = User.new
+  end
+  def create
+    user = User.new(user_params)
+
+    if user.save
+    else
+    end
+  end
+  def destroy
+    user = User.find(params[:id])
+    user.delete
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :username, :password, :password_confirmation)
+  end
+end
