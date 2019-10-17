40e57bef8384743860d831a53622e423b5e5be8f
bghalami <benjaminghalami@gmail.com>
2019-10-17 13:25:52 -0600

    Updated migration and added validations to user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,7 @@
 class User < ApplicationRecord
+  validates_presence_of :title
+  validates_presence_of :username, uniqueness: true
+
   belongs_to :company
+
 end
--- db/migrate/20191009233656_create_user_table.rb
+++ db/migrate/20191009233656_create_user_table.rb
@@ -1,13 +1,13 @@
 class CreateUserTable < ActiveRecord::Migration[5.2]
   def change
     create_table :user_tables do |t|
-      t.string :name
-      t.string :username
-      t.string :password_digest
-      t.float :pay_rate
-      t.integer :hours_requested
-      t.integer :role
-      t.references :company, foreign_key: true
+      t.string :name, null: false
+      t.string :username, null: false
+      t.string :password_digest, null: false
+      t.float :pay_rate, null: false
+      t.integer :hours_requested, null: false
+      t.integer :role, null: false
+      t.references :company, foreign_key: true, null: false
     end
   end
 end
