4ab2956793df9a04891149397a4d8359df27a1de
bghalami <benjaminghalami@gmail.com>
2019-10-17 13:31:37 -0600

    Added enum for roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ApplicationRecord
-  validates_presence_of :title
+  validates_presence_of :name, :pay_rate, :hours, :role
   validates_presence_of :username, uniqueness: true
+
+  has_secure_password
 
   belongs_to :company
 
+  enum role: { employee: 0, manager: 1, admin: 2 }
 end
